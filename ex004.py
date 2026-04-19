20eb4c2124834f735441dd07cedfd417971fa245
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2026-04-19 14:25:55 -0300

    Dissecando uma Variável

---
+++ ex004.py
@@ -0,0 +1,10 @@
+algo = input('Digite algo: ')
+
+print('O tipo primitivo desse valor é ', type(algo))
+print('Só tem espaços? ', algo.isspace())
+print('É número? ', algo.isnumeric())
+print('É alfabético? ', algo.isalpha())
+print('É alfanumérico? ', algo.isalnum())
+print('Está em maiúsculas? ', algo.isupper())
+print('Está em minusculas? ', algo.islower())
+print('Está capitalizado? ', algo.istitle())
