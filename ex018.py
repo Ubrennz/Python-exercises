d13a5faa1b015243dd534cb1f759607a685055c2
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2026-04-23 16:25:17 -0300

    Seno, cosseno e tangente

---
+++ ex018.py
@@ -0,0 +1,8 @@
+from math import sin, cos, tan
+
+angulo = float(input('Digite um ângulo qualquer: '))
+
+print(f'O seno de {angulo} é de {sin(angulo)}')
+print(f'O cosseno de {angulo} é de {cos(angulo)}')
+print(f'A tangente de {angulo} é de {tan(angulo)}')
+
